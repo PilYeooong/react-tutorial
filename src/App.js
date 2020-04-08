7dcaf06c3f52a533faa1f92296b02219618d78c5
PilYeong <pilyeooong@gmail.com>
2020-04-08 22:27:29 +0900

    prop, propType, 컴포넌트 예시

--- src/App.js
+++ src/App.js
@@ -2,46 +2,71 @@ import React from 'react';
 
 import 'App.css';
 import { Button } from 'antd';
+import PropTypes from "prop-types"
+import ThemedButton from "ThemedButton";
 
-const actions = {
-  init(initialValue) {
-    return { value: initialValue };
-  },
-  increment(prevState) {
-    return { value: prevState.value + 1};
-  },
-  decrement(prevState){
-    return { value: prevState.value - 1};
-  },
-};
 
-class Counter1 extends React.Component {
-  state = actions.init(this.props.initialValue);
-  
-  // constructor(props) {
-  //   super(props);
-  //   this.state = actions.init(this.props.initialValue);
-  // }
+class PostDetail extends React.Component {
+  static propTypes = {
+    postId: PropTypes.number.isRequired,
+  }
 
-  render() {
-    const { value } = this.state;
+  state = {
+    PostDetail: null,
+  }
+
+  componentDidMount() {
+    const { postId } = this.props;
+    this.requestPost(postId);
+  }
+  componentDidUpdate(prevProps) {
+    const { postId } = this.props;
+    if ( postId !== prevProps.postId ){
+      this.requestPost(postId);
+    }
+  }
+
+  requestPost(postId){
+    console.log(`request post #${postId}`)
+    // axios (http client) => this.setState를 통한 반영
+    this.setState({
+      postDetail: null
+    });
+    setTimeout(() => {
+      this.setState({
+        postDetail: `로딩된 post #${postId}`
+      })
+    }, 3000);
+  }
+
+  render () {
+    const { postId } = this.props;
+    const { postDetail } = this.state;
     return (
       <div>
-        Counter1: {value}
-        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
-        <Button onClick={() => this.setState(actions.increment)}>-1</Button>
+        포스팅 #{postId}
+        <hr />
+        {!postDetail && "로딩 중 ..."}
+        {postDetail}
       </div>
-    );
+    )
   }
 }
 
 
-function App() {
-  return (
-    <div>
-      <Counter1 initialValue={10}></Counter1>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    postId: 10,
+  }
+  render() {
+    return (
+      <div>
+        <ThemedButton theme="success" label="Say Hello"></ThemedButton>
+        <PostDetail postId={this.state.postId}></PostDetail>
+        <button onClick={() => this.setState({ postId: 20 })}>변경하기</button>
+      </div>
+    );
+  }
 }
 
 export default App;
