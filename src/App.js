fdd48d8b3a9e5b91f6f2106df6cb82c83624b316
PilYeong <pilyeooong@gmail.com>
2020-04-09 13:38:59 +0900

    이벤트 처리하기

--- src/App.js
+++ src/App.js
@@ -1,70 +1,31 @@
 import React from 'react';
 
 import 'App.css';
-import { Button } from 'antd';
+import Counter from 'Counter';
 import PropTypes from "prop-types"
-import ThemedButton from "ThemedButton";
-
-
-class PostDetail extends React.Component {
-  static propTypes = {
-    postId: PropTypes.number.isRequired,
-  }
-
-  state = {
-    PostDetail: null,
-  }
-
-  componentDidMount() {
-    const { postId } = this.props;
-    this.requestPost(postId);
-  }
-  componentDidUpdate(prevProps) {
-    const { postId } = this.props;
-    if ( postId !== prevProps.postId ){
-      this.requestPost(postId);
-    }
-  }
-
-  requestPost(postId){
-    console.log(`request post #${postId}`)
-    // axios (http client) => this.setState를 통한 반영
-    this.setState({
-      postDetail: null
-    });
-    setTimeout(() => {
-      this.setState({
-        postDetail: `로딩된 post #${postId}`
-      })
-    }, 3000);
-  }
-
-  render () {
-    const { postId } = this.props;
-    const { postDetail } = this.state;
-    return (
-      <div>
-        포스팅 #{postId}
-        <hr />
-        {!postDetail && "로딩 중 ..."}
-        {postDetail}
-      </div>
-    )
-  }
-}
-
 
 class App extends React.Component{
   state = {
-    postId: 10,
+    myquery: "",
+    lang:"",
   }
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    })
+    console.log("chaged value:" , value, this);
+  };
+
   render() {
     return (
-      <div>
-        <ThemedButton theme="success" label="Say Hello"></ThemedButton>
-        <PostDetail postId={this.state.postId}></PostDetail>
-        <button onClick={() => this.setState({ postId: 20 })}>변경하기</button>
-      </div>
+      <>
+        <Counter onClick={() => console.log("clicked")}/>
+        <input name="myquery" onChange={this.onChange} />
+        <input name="lang" onChange={this.onChange} />
+        <hr />
+        {JSON.stringify(this.state)}
+      </>
     );
   }
 }
